0f04f10b8928fd484968cac94f11c899ff01678b
nicolaair <poraduem@gmail.com>
2020-12-01 23:46:08 +0300

    implement task 5

--- src/tasks/Task5.java
+++ src/tasks/Task5.java
@@ -5,9 +5,9 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
 Задача 5
@@ -17,10 +17,11 @@ import java.util.Map;
 Конвертер одной персоны дополнен!
  */
 public class Task5 implements Task {
-
   // !!! Редактируйте этот метод !!!
   private List<ApiPersonDto> convert(List<Person> persons, Map<Integer, Integer> personAreaIds) {
-    return new ArrayList<>();
+    return persons.stream()
+            .map(person -> convert(person, personAreaIds.get(person.getId())))
+            .collect(Collectors.toList());
   }
 
   private static ApiPersonDto convert(Person person, Integer areaId) {
@@ -29,6 +30,7 @@ public class Task5 implements Task {
     dto.setId(person.getId().toString());
     dto.setName(person.getFirstName());
     dto.setAreaId(areaId);
+
     return dto;
   }
 
@@ -37,6 +39,7 @@ public class Task5 implements Task {
     Person person1 = new Person(1, "Name", Instant.now());
     Person person2 = new Person(2, "Name", Instant.now());
     Map<Integer, Integer> personAreaIds = Map.of(1, 1, 2, 2);
+
     return List.of(convert(person1, 1), convert(person2, 2))
         .equals(convert(List.of(person1, person2), personAreaIds));
   }
