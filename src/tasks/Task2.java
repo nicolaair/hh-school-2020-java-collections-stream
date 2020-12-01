5d4bec167805bbac50019c1173cf12c9e2ffd152
nicolaair <poraduem@gmail.com>
2020-12-01 23:03:49 +0300

    implement task 2

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -4,11 +4,9 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -17,12 +15,14 @@ import java.util.stream.Collectors;
 отсортировать персоны по дате создания и выдать первые limit штук.
  */
 public class Task2 implements Task {
-
-  // !!! Редактируйте этот метод !!!
+  // O(n)
   private static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                       Collection<Person> persons2,
                                                       int limit) {
-    return new ArrayList<>();
+    return Stream.concat(persons1.stream(), persons2.stream())
+            .sorted(Comparator.comparing(person -> person.getCreatedAt().getEpochSecond()))
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 
   @Override
@@ -36,6 +36,7 @@ public class Task2 implements Task {
         new Person(3, "Person 3", time.minusSeconds(1)),
         new Person(4, "Person 4", time.plusSeconds(2))
     );
+
     return combineAndSortWithLimit(persons1, persons2, 3).stream()
         .map(Person::getId)
         .collect(Collectors.toList())
